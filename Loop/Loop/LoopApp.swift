140772070267296857f1f3d6df8138605bf0839f
shriramBaNW <72285290+Shriram-Vasudevan@users.noreply.github.com>
2024-09-30 16:32:17 -0400

    First commit

---
+++ Loop/Loop/LoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopApp.swift
+//  Loop
+//
+//  Created by Shriram Vasudevan on 9/29/24.
+//
+
+import SwiftUI
+
+@main
+struct LoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Loop/Loop/Utilities/UserCloudKitUtility.swift
@@ -0,0 +1,19 @@
+//
+//  UserCloudKitUtility.swift
+//  Loop
+//
+//  Created by Shriram Vasudevan on 9/29/24.
+//
+
+import Foundation
+import CloudKit
+
+class UserCloudKitUtility {
+    static let container = CloudKit.CKContainer(identifier: "ICloud.LoopContainer")
+    
+    static func addUserData() {
+        let privateDB = container.privateCloudDatabase
+        
+        let userRecord = CKRecord(recordType: "UserRecord")
+    }
+}
